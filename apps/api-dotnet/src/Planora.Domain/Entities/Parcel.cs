2f5b43605f41bad2ae14e9c234c77e495842ed63
Donia Shaban <doniashaban723@gmail.com>
2026-06-05 13:06:49 +0300

    refactor(domain): reorganize identity entities and introduce schemas
    - Move AuthAuditLog, OtpRecord, and RefreshToken to Common folder
    - Relocate Parcel entity to its own dedicated domain folder
    - Add foundational domain enums and entity-specific error records

--- apps/api-dotnet/src/Planora.Domain/Entities/Parcel.cs
+++
@@ -1,8 +0,0 @@
-using Planora.Domain.Shared.Abstractions;
-
-namespace Planora.Domain.Entities;
-
-public class Parcel : SoftDeletableEntity
-{
-    
-}
---
+++ apps/api-dotnet/src/Planora.Domain/Enums/ModuleType.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Planora.Domain.Enums
+{
+    public enum ModuleType { 
+        Topographic, 
+        SoilComposition, 
+        BearingCapacity, 
+        ConstructionRisk, 
+        BoreholeOptimization, 
+        PdfReport 
+    }
+}
