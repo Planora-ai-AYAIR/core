f53bf80c239f4f1cf441291245aac99a257e278e
Ziad Mohammed <00ziadmohammed606@gmail.com>
2026-06-09 08:42:53 +0300

    Add background job service and Hangfire log override
    - Add IBackgroundJobService interface and implementation using Hangfire
    - Update logging config to set Hangfire log level to Warning

---
+++ apps/api-dotnet/src/Planora.Infrastructure/Services/BackgroundJobService.cs
@@ -0,0 +1,20 @@
+﻿using Hangfire;
+using Planora.Application.Interfaces.Services;
+using System.Linq.Expressions;
+
+namespace Planora.Infrastructure.Services;
+
+public class BackgroundJobService(IBackgroundJobClient backgroundJobClient, IRecurringJobManager recurringJobManager) : IBackgroundJobService
+{
+    public string Enqueue<T>(Expression<Action<T>> methodCall) =>
+        backgroundJobClient.Enqueue(methodCall);
+
+    public string Enqueue<T>(Expression<Func<T, Task>> methodCall) =>
+        backgroundJobClient.Enqueue(methodCall);
+
+    public void Schedule<T>(Expression<Action<T>> methodCall, TimeSpan delay) =>
+        backgroundJobClient.Schedule(methodCall, delay);
+
+    public void AddOrUpdateRecurring<T>(string jobId, Expression<Action<T>> methodCall, string cronExpression) =>
+        recurringJobManager.AddOrUpdate(jobId, methodCall, cronExpression);
+}
