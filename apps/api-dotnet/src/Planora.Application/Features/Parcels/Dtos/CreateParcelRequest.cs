4cc09e302090f90fc0750cb70d96a6097843c43d
Donia Shaban <doniashaban723@gmail.com>
2026-06-09 20:50:47 +0300

    feat(jobs): integrate hangfire topography queue
    - Add topography background job contract
    - Add Hangfire job implementation
    - Register job services in DI

--- apps/api-dotnet/src/Planora.Application/Features/Parcels/Dtos/CreateParcelRequest.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Text.Json;
-
-namespace Planora.Application.Features.Parcels.Dtos;
-
-public sealed record CreateParcelRequest(
-    string Name,
-    JsonElement GeoJson,
-    decimal Area,
-    string AreaUnit);
---
+++ apps/api-dotnet/src/Planora.Application/Interfaces/Jobs/IProcessTopographyJob.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Planora.Application.Interfaces.Jobs
+{
+    public interface IProcessTopographyJob
+    {
+        string Enqueue(Guid parcelId);
+    }
+}
